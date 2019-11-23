f562870aa6d87c2c316a6879c11ccb34565b619b
Jefferson Costa Gomes <31323813+JeffehCG@users.noreply.github.com>
2019-11-23 20:53:31 -0300

    Configuração inicial

---
+++ API_ListaCompra/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_ListaCompra.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok("Index API ");
+        }
+    }
+}
